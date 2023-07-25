21ed5512ccc27a12ae48881166463b2179cec098
shangyc <15038311053@163.com>
2023-07-25 10:36:49 +0800

    观察者模式

---
+++ structural_patterns/mediator/mediator.go
@@ -0,0 +1,3 @@
+// mediator 中介者模式
+// https://refactoringguru.cn/design-patterns/mediator?_gl=1*ac44gv*_ga*Njc3MzQzNTA5LjE2ODk3NTc4NjE.*_ga_SR8Y3GYQYC*MTY4OTgxNzkyMy40LjAuMTY4OTgxNzkyMy42MC4wLjA.
+package main
